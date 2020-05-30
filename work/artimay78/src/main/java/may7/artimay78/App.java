7bdda5cef8851db5e6fc8dbfa3bc1e616d53cc1e
shebahrajadurai <shebah_raja@yahoo.com>
2020-05-29 18:21:13 -0700

    artimay78 proj

---
+++ work/artimay78/src/main/java/may7/artimay78/App.java
@@ -0,0 +1,13 @@
+package may7.artimay78;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ work/artimay78/src/test/java/may7/artimay78/may7testcas.java
@@ -0,0 +1,30 @@
+package may7.artimay78;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class may7testcas {	
+	
+	
+	    private WebDriver driver;	
+	    
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		
+		@BeforeTest
+		public void beforeTest() {	
+		    driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
